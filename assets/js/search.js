38fe88fa76e255d5af9242b7c726cb9b238d05d6
Stephen Ajulu <alunje73@gmail.com>
2024-03-11 18:17:31 +0300

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,50 +1,118 @@
-function displayResults (results, store) {
-  const searchResults = document.getElementById('results')
-  if (results.length) {
-    let resultList = ''
-    // Iterate and build result list elements
-    for (const n in results) {
-      const item = store[results[n].ref]
-      resultList += '<li><p><a href="' + item.url + '">' + item.title + '</a></p>'
-      resultList += '<p>' + item.content.substring(0, 150) + '...</p></li>'
+window.addEventListener("DOMContentLoaded", () => {
+
+    "use strict";
+    let index, parse, query;
+
+    const form = document.getElementById("search");
+    const input = document.getElementById("search-input");
+
+    form.addEventListener(
+        "submit",
+        function (event) {
+            event.preventDefault();
+
+            const keywords = input.value.trim();
+            if (!keywords) return;
+
+            query = keywords;
+            initSearchIndex();
+        },
+        false,
+    );
+
+    function handleEvent(e) {
+        console.log(e.type);
     }
-    searchResults.innerHTML = resultList
-  } else {
-    searchResults.innerHTML = 'No results found.'
-  }
-}
 
-// Get the query parameter(s)
-const params = new URLSearchParams(window.location.search)
-const query = params.get('query')
+    async function initSearchIndex() {
+        const request = new XMLHttpRequest();
+        request.open("GET", "/index.json");
+        request.responseType = "json";
+        request.addEventListener (
+            "load",
+            function () {
+                parse = {};
+                index = lunr(function () {
 
-// Perform a search if there is a query
-if (query) {
-  // Retain the search input in the form when displaying results
-  document.getElementById('search-input').setAttribute('value', query)
+                    const documents = request.response;
 
-  const idx = lunr(function () {
-    this.ref('id')
-    this.field('title', {
-      boost: 15
-    })
-    this.field('tags')
-    this.field('content', {
-      boost: 10
-    })
+                    this.ref("href");
+                    this.field("title", {
+                        boost: 20,
+                        usePipeline: true,
+                        wildcard: lunr.Query.wildcard.TRAILING,
+                        presence: lunr.Query.presence.REQUIRED,
+                    });
+                    this.field("content", {
+                        boost: 15,
+                    });
+                    this.field("summary", {
+                        boost: 10,
+                    });
+                    this.field("tags", {
+                        boost: 5,
+                    });
 
-    for (const key in window.store) {
-      this.add({
-        id: key,
-        title: window.store[key].title,
-        tags: window.store[key].category,
-        content: window.store[key].content
-      })
+                    documents.forEach(function(doc) {
+                      this.add(doc);
+                      parse[doc.href] = {
+                        title: doc.title,
+                        content: doc.content,
+                        summary: doc.summary,
+                        tags: doc.tags,
+                      };
+                    }, this);
+                });
+                search(query);
+            },
+            false,
+        );
+        request.addEventListener("error", handleEvent);
+        request.send(null);
     }
-  })
 
-  // Perform the search
-  const results = idx.search(query)
-  // Update the list with results
-  displayResults(results, window.store)
-}
+    function search(keywords) {
+        const results = index.search(keywords);
+
+        if ("content" in document.createElement("template")) {
+
+          const target = document.querySelector(".is-search-result");
+
+          while (target.firstChild) target.removeChild(target.firstChild);
+
+          const title = document.createElement("h3");
+          title.id = "search-results";
+          title.className = "subtitle is-size-3";
+
+          if (results.length == 0)
+              title.textContent = `No results found for "${keywords}"`;
+          else if (results.length == 1)
+              title.textContent = `Found one result for "${keywords}"`;
+          else
+              title.textContent = `Found ${results.length} results for "${keywords}"`;
+
+          target.appendChild(title);
+          document.title = title.textContent;
+
+          const template = document.getElementById("is-search-template");
+
+          results.forEach(function(result) {
+            const doc = parse[result.ref];
+            const element = template.content.cloneNode(true);
+
+            element.querySelector(".is-read-more")
+                .href = doc.href;
+            element.querySelector(".is-read-more")
+                .textContent = doc.title;
+            element.querySelector(".summary")
+                .textContent = doc.summary;
+            element.querySelector(".tags")
+                .textContent = doc.tags;
+            target.appendChild(element);
+
+          }, this);
+        } else {}
+    }
+  },
+  false,
+);
